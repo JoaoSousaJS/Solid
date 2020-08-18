1e7e90e2d7b7a0a39ff0592122a60f868390ea58
Joao <joao.rsousa23@gmail.com>
2020-08-18 23:38:01 +0930

    refactor: change import

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,19 +1,20 @@
-import { httpResponse } from '../protocols/http'
-import { ServerError } from '../errors/server-error'
 
-export const badRequest = (error: Error): httpResponse => ({
+import { ServerError } from '../errors'
+import { HttpResponse } from '../protocols/http'
+
+export const badRequest = (error: Error): HttpResponse => ({
 
   statusCode: 400,
   body: error
 })
 
-export const serverError = (): httpResponse => ({
+export const serverError = (): HttpResponse => ({
 
   statusCode: 500,
   body: new ServerError()
 })
 
-export const ok = (data: any): httpResponse => ({
+export const ok = (data: any): HttpResponse => ({
 
   statusCode: 200,
   body: data
