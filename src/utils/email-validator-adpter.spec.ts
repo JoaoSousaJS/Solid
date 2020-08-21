753306d4e634519a423981cf82933e157940070b
Joao <joao.rsousa23@gmail.com>
2020-08-21 14:20:19 +0930

    refactor: quick fix

--- src/utils/email-validator-adpter.spec.ts
+++ src/utils/email-validator-adpter.spec.ts
@@ -18,17 +18,13 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
-})
 
-describe('EmailValidator Adapter', () => {
   test('Should return true if validator returns true ', () => {
     const sut = makeSut()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
-})
 
-describe('EmailValidator Adapter', () => {
   test('Should call validator with correct email ', () => {
     const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
