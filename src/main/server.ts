84dea0f1153dedc5d17d7cc350bc13d436546512
Joao <joao.rsousa23@gmail.com>
2020-09-03 14:26:28 +0930

    refactor: change routes to use filesystem intead of fast-glob

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,9 +3,6 @@ import env from './config/env'
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
-    console.log(process.env.MONGO_URL)
-    console.log(env.mongoUrl)
-    console.log(env.port)
     const app = (await import('./config/app')).default
     app.listen(env.port, () => console.log(`Server Running at http://localhost:${env.port}`))
   })
